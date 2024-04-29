c231a98662e99ef9a180a1fbf1bab00ab0cb059b
StevenJM05 <116135869+StevenJM05@users.noreply.github.com>
2024-04-28 19:15:05 -0600

    Configuracion de controlador alumno

--- controladores/alumnos_controller.php
+++ controladores/alumnos_controller.php
@@ -2,11 +2,12 @@
 require_once("conexion.php");
 
 class alumnos_controller extends Conexion{
-    public function agregar($idcarre,$nom,$ape,$dir,$tel){
-        $sql="INSERT INTO alumnos (id_carrera, nombres, apellidos, direccion, telefono) VALUES ('".$idcarre."','".$nom."','".$ape."','".$dir."','".$tel."')";
-        $rs = $this -> ejecutarSQL($sql);
+    public function agregar($alumnos) {
+        $sql = "INSERT INTO alumnos (id_carrera, nombres, apellidos, direccion, telefono) 
+                VALUES ('{$alumnos->getIdCarrera()}', '{$alumnos->getNombres()}', '{$alumnos->getApellidos()}', '{$alumnos->getDireccion()}', '{$alumnos->getTelefono()}')";
+        $rs = $this->ejecutarSQL($sql);
     }
-
+    
     public function listar(){
         $sql = "SELECT * FROM alumnos";
         $rs= $this -> ejecutarSQL($sql);
@@ -16,17 +17,6 @@ class alumnos_controller extends Conexion{
         }
         return $resultado;
     }
-
-    /*public function eliminar(){
-        $idal="";
-        $sql = "DELETE FROM alumnos WHERE id_alumno=$idal";
-        $rs= $this -> ejecutarSQL($sql);
-        $resultado=array();
-        while($fila=$rs->fetch_assoc()){
-         $resultado[]=new Alumnos ($fila["id_alumno"],$fila["id_carrera"],$fila["nombres"],$fila["apellidos"],$fila["direccion"],$fila["telefono"]);
-        }
-        return $resultado;
-    }*/
 }
 
 ?>
