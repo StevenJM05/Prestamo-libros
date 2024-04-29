46091d4d1f2e737a9df70200a37b356522fef91d
AlexZepeda19 <manuel.zepeda23@itca.edu.sv>
2024-04-29 06:45:10 +0200

    Correciones de libro

--- index.php
+++ index.php
@@ -2,10 +2,13 @@
 if (!defined("URL")) define("URL","http://localhost/Prestamo-libros/");
 
 require_once("controladores/contenido.php");
+
 require_once("controladores/alumnos_controller.php");
 require_once("modelos/alumnos.php");
+
 require_once("controladores/carreras_controller.php");
 require_once("modelos/carreras.php");
+
 require_once("controladores/escuelas_controller.php");
 require_once("modelos/escuelas.php");
 
