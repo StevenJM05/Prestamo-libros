18983cada051c7b809576a869def3cb8470cdf64
G3RS0N78 <168381297+G3RS0N78@users.noreply.github.com>
2024-04-29 19:55:16 -0600

    borre una conexion porque habian dos

--- controladores/conexion.php
+++ controladores/conexion.php
@@ -1,17 +1,12 @@
 <?php
 date_default_timezone_set('America/El_Salvador');
 
-<<<<<<< HEAD
-    if (!defined('SERVIDOR')) define('SERVIDOR', 'localhost');
-    if (!defined('USUARIO')) define('USUARIO', 'root');
-    if (!defined('CLAVE')) define('CLAVE', '');
-    if (!defined('DATABASE')) define('DATABASE', 'EXAMEN2PHP');
-=======
+
 if (!defined('SERVIDOR')) define('SERVIDOR', 'localhost');
 if (!defined('USUARIO')) define('USUARIO', 'root');
 if (!defined('CLAVE')) define('CLAVE', '');
 if (!defined('DATABASE')) define('DATABASE', 'EXAMEN2PHP');
->>>>>>> 9571dbe13f8dfef47ba7f89257bf0879f56eb0da
+
 
 
 class conexion
