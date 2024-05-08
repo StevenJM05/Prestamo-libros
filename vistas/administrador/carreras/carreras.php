0fb602c97c31fa2d7c424feda3cee6b33a871f45
StevenJM05 <116135869+StevenJM05@users.noreply.github.com>
2024-05-07 23:11:06 -0600

    Revert "design alumno y carrera"
    This reverts commit 03812240332251e7bfcf4238d7353c2704bcd7e7.

--- vistas/administrador/carreras/carreras.php
+++ vistas/administrador/carreras/carreras.php
@@ -1,41 +1,67 @@
 <?php
 $carreras_controller = new carreras_controller();
 
-if (isset($_POST['ok1'])) {
-
+if(isset($_POST['ok1'])){
+    
     $carreras = new carreras("", "", $_POST['nombre_carrera'], $_POST['asignaturas']);
     $carreras_controller->agregar($idcarrera, $idescuela, $nom_carrera, $aginaturas);
+
 }
 ?>
 
 <?php
-if (isset($_POST['del'])) {
+ if(isset($_POST['del'])){
 
-    if (isset($_POST['eliminar']) && is_array($_POST['eliminar'])) {
-        foreach ($_POST['eliminar'] as $idcarreras) {
-            $carreras_controller->delete(intval($idcarreras));
+    if(isset($_POST['eliminar']) && is_array($_POST['eliminar'])) {
+        foreach($_POST['eliminar'] as $idcarreras) {
+            $carreras_controller->delete(intval($idcarreras)); 
+            
         }
     }
+
 }
 ?>
-<body>
+<body style="background-color: #f0f0f0;">
+
 <div class="container mt-5 text-center">
-        <h1 class="fw-bold"s>LISTAR CARRERAS</h1>
+        <h1 class="fw-bold">INFORMACIÓN DE CARRERAS</h1>
     </div>
 
- 
+    <div class="container mt-5 position-relative" style="margin-left: 210px; margin-top: -15px;">
+    <a class='btn btn-success position-absolute top-0 start-0' href='addcarreras'>Agregar Carreras</a>
+</div>
 
-                            echo "
+</div>
+<br><br>
+
+<form method=post >
+<table class="table table-dark mx-auto" style="width: 80%;">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">ID Carreras</th>
+                <th scope="col">Nombre de la Escuela</th>
+                <th scope="col">Nombre de la carrera</th>
+                <th scope="col"># Asignaturas</th>
+                <th scope="col">Actualizar</th>
+            </tr>
+        </thead>
+        <tbody>
+
+        <?php 
+        foreach ($carreras_controller->listar() as $aggal) {
+
+            echo "
                         <tr  >
-                            <td  > <input type='checkbox'  name='eliminar[]' value='" . $aggal->getIdcarrera() . "' title=' ' > </td>
+                            <td  > <input type='checkbox'  name='eliminar[]' value='".$aggal->getIdcarrera()."' title=' ' > </td>
                             <td>" . $aggal->getIdcarrera() . "</td>
                             <td>" . $aggal->getIdescuelas() . "</td>
                             <td>" . $aggal->getNombrecarrera() . "</td>
                             <td>" . $aggal->getAsignaturas() . "</td>
-                            <td><a href='up_carreras/" .  $aggal->getIdcarrera()  . " '>Click para actualizar </a></td>
+                            <td><a href='up_carreras/" .  $aggal->getIdcarrera()  . " '>MODIFICAR </a></td>
                         </tr>
                         ";
-                        }
+    
 
            } 
             
@@ -51,13 +77,9 @@ if (isset($_POST['del'])) {
             </tr>
         </tbody>
     </table>
-       <div class="container mt-6 position-relative" style="margin-right: 50px">
-    <a class='btn btn-primary position-absolute top-0 start-0' href='addcarreras'>Agregar Carreras</a>
-      </div>
     </form>
 </div>
 
 
-</div>
 </div>
 </div>
